420b4e0bda39d6fe54ffe6187af75bcc1c5e8609
Nguyễn Đình Khôi <49549370+ngdkhoi@users.noreply.github.com>
2022-01-09 20:54:15 +0700

    first commit

---
+++ ORM-Framework-DP/ORM-Framework-DP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ORM_Framework_DP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
