55272dca5bd001fa2cb9ed7fea16e319a70b9f7b
Nguyen Duong <nguyenduongag@gmail.com>
2022-01-16 15:53:32 +0700

    done select

---
+++ ORM-Framework-DP/ORM-Framework-DP/DatabaseSyntax/DatabaseSyntax.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ORM_Framework_DP
+{
+    public interface DatabaseSyntax
+    {
+        string GetConnectionString(string host, string dbName, string port, string uid, string password);
+        string GetWherePart(string condition);
+        string GetOr();
+        string GetAnd();
+        string GetSelectAllPart(string tableName);
+        string GetGroupByPart(List<string> columeNames);
+        string GetHavingPart(string condition);
+        string BuildSelectWhereFromValuePairs(Dictionary<string, string> columeNameValuePairs, string tableName);
+        string ConvertValueToString(object value, Type type);
+
+    }
+}
